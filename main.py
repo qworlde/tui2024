2e306f4674f582a28d883bb1b9bbdf868e079576
maharamia <101996964+qworlde@users.noreply.github.com>
2024-01-10 13:43:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 
 W, H = 800, 600
-WB, HB = 50, 525
+WB, HB = 50, 475
 WM, HM = 150, 120
 WK, HK = 150, 170
 HK2 = 220
@@ -22,9 +22,8 @@ tr = pygame.image.load('tlv1.png').convert_alpha()
 r_tr = tr.get_rect(center = (50, 525))
 
 #патрон
-bullet = pygame.Surface((3, 6))
-bullet.fill((255, 255, 255))
-r_bullet = bullet.get_rect(center = (50, 525))
+bullet = pygame.image.load('bull2.png')
+r_bullet = bullet.get_rect(center = (50, 475))
 bullet.set_alpha(0)
 
 #score
@@ -77,7 +76,12 @@ for i in range(11):
 
 speed_tr = 3
 speed_bullet = 5
+speed_m = 0.5
+p = 1
 fb = 0
+c = 0
+v = 0
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -85,22 +89,119 @@ while True:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        r_tr.x -= speed_tr
+        if r_tr.x >= 0:
+            r_tr.x -= speed_tr
     elif keys[pygame.K_RIGHT]:
-        r_tr.x += speed_tr
+        if r_tr.x <= 725:
+            r_tr.x += speed_tr
     if keys[pygame.K_SPACE] and fb == 0:
         fb = 1
         bullet.set_alpha(255)
-        r_bullet.x = r_tr.x + 37
+        r_bullet.x = r_tr.x + 18
 
     if fb == 1:
         r_bullet.y -= speed_bullet
         if r_bullet.y < 0:
             fb = 0
             bullet.set_alpha(0)
-            r_bullet.x = r_tr.x + 37
+            r_bullet.x = r_tr.x + 18
             r_bullet.y = HB
 
+    if p == 1:
+        for n in n4i:
+            if n.rect.x >= 740:
+                c += 1
+                for n1 in n4i:
+                    n1.rect.y += 50
+                break
+            else:
+                n.rect.x += 0.5
+        
+        for kr in krabi:
+            if kr.rect.x == 740:
+                c += 1
+                for kr1 in krabi:
+                    kr1.rect.y += 50
+                break
+            else:
+                kr.rect.x += 0.5
+
+        for med in medusi:
+            if med.rect.x >= 740:
+                c += 1
+                for med1 in medusi:
+                    med1.rect.y += 50
+                break
+            else:
+                med.rect.x += 0.5
+    else:
+        print(p)
+        for n in n4i:
+            n.rect.x -= 0.5
+            if n.rect.x <= 0:
+                v += 1
+                for n1 in n4i:
+                    n1.rect.y += 50
+                break
+            else:
+                n.rect.x -= 0.5
+
+        for kr in krabi:
+            kr.rect.x -= 0
+            if kr.rect.x <= 0:
+                v += 1
+                for kr1 in krabi:
+                    kr1.rect.y += 50
+                break
+            else:
+                kr.rect.x -= 0.5
+
+        for med in medusi:
+            med.rect.x -= 0.5
+            if med.rect.x <= 0:
+                v += 1
+                for med1 in medusi:
+                    med1.rect.y += 50
+                break
+            else:
+                med.rect.x -= 0.5
+
+    if c == 3:
+        p = 0
+        c = 0
+
+    if v == 3:
+        p = 1
+        v = 0
+        
+    for n in n4i:
+        if r_bullet.collidepoint(n.rect.center):
+            n.kill()
+            fb = 0
+            bullet.set_alpha(0)
+            r_bullet.x = r_tr.x + 18
+            r_bullet.y = HB
+            score += 10
+
+    for kr in krabi:
+        if r_bullet.collidepoint(kr.rect.center):
+            kr.kill()
+            fb = 0
+            bullet.set_alpha(0)
+            r_bullet.x = r_tr.x + 18
+            r_bullet.y = HB
+            score += 20
+    
+    for med in medusi:
+        if r_bullet.collidepoint(med.rect.center):
+            med.kill()
+            fb = 0
+            bullet.set_alpha(0)
+            r_bullet.x = r_tr.x + 18
+            r_bullet.y = HB
+            score += 40
+
+    scorep = f.render(str(score), 1, (0, 255, 0))
     sc.blit(surf, (0, 0))
     sc.blit(scb, (10, 10))
     sc.blit(scorep, (110, 10))
